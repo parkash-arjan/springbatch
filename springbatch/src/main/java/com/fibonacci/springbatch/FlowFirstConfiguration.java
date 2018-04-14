7af02f4ae4f496d59154399869bc69b1c9532fe8
parkash-arjan <parkash.arjan.github@gmail.com>
2018-04-14 18:23:39 -0400

    concurrent step execution using spilt and SimpleAsynchTaskExecutor

--- springbatch/src/main/java/com/fibonacci/springbatch/FlowFirstConfiguration.java
+++ springbatch/src/main/java/com/fibonacci/springbatch/FlowFirstConfiguration.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableBatchProcessing
+//@Configuration
+//@EnableBatchProcessing
 public class FlowFirstConfiguration {
 
 	@Autowired
---
+++ springbatch/src/main/java/com/fibonacci/springbatch/SplitBatchConfiguration.java
@@ -0,0 +1,73 @@
+package com.fibonacci.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+@EnableBatchProcessing
+public class SplitBatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Flow flow1() {
+
+		Step step1 = stepBuilderFactory.get("step1").tasklet(tasklet()).build();
+
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+		Flow flow = flowBuilder.start(step1).build();
+
+		return flow;
+	}
+
+	@Bean
+	public Flow flow2() {
+
+		Step step2 = stepBuilderFactory.get("step2").tasklet(tasklet()).build();
+		Step step3 = stepBuilderFactory.get("step3").tasklet(tasklet()).build();
+
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+		Flow flow = flowBuilder.start(step2).next(step3).build();
+
+		return flow;
+	}
+
+	@Bean
+	public Job job() {
+
+		Job job = jobBuilderFactory.get("job").start(flow1()).split(new SimpleAsyncTaskExecutor()).add(flow2()).end().build();
+		return job;
+	}
+
+	@Bean
+	public Tasklet tasklet() {
+		return new CountingTasklet();
+	}
+
+	public static class CountingTasklet implements Tasklet {
+		@Override
+		public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+			System.out.println(String.format("%s has been executed on thread %s", chunkContext.getStepContext().getStepName(), Thread.currentThread().getName()));
+			return RepeatStatus.FINISHED;
+		}
+
+	}
+
+}
