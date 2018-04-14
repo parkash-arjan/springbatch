4ad4b1aa89361d623e4b454419df0836b9367951
parkash-arjan <parkash.arjan.github@gmail.com>
2018-04-14 14:35:55 -0400

    Project Added

---
+++ springbatch/src/main/java/com/fibonacci/springbatch/SpringbatchApplication.java
@@ -0,0 +1,12 @@
+package com.fibonacci.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchApplication.class, args);
+	}
+}
